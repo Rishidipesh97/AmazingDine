b88b596bec443b630650087ca3abebf6d863d850
dipesh_rishi <36751695+Rishidipesh97@users.noreply.github.com>
2020-03-08 10:10:56 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,12 +1,24 @@
-<?php
-// After uploading to online server, change this connection accordingly
-
-$con = mysqli_connect("localhost","id3269709_root","9814800439","id3269709_esdr");
-
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-
-
-?>
+<?php
+
+// After uploading to online server, change this connection accordingly
+
+
+
+$con = mysqli_connect("localhost","id3269709_root","********","id3269709_esdr");
+
+
+
+if (mysqli_connect_errno())
+
+  {
+
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+
+  }
+
+
+
+
+
+?>
+
